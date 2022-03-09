dd2e274027b73dd38120c4b25613e0e37f0e89f1
skarltjr <kisa0828@naver.com>
2022-03-09 21:25:07 +0900

    chore : test3

--- src/main/java/com/example/ci_cd/hello/HelloController.java
+++ src/main/java/com/example/ci_cd/hello/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("")
     public ResponseEntity hello() {
-        // test 2
+        // test 3
         return ResponseEntity.ok().body("hello");
     }
 }
